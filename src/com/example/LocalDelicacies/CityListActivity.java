d94b3de1e4cba562167a9afe5ad22e4b2c1578ca
Milan Landaverde <mlandaverde@mobiquityinc.com>
2014-07-17 15:51:05 -0400

    init commit

---
+++ src/com/example/LocalDelicacies/CityListActivity.java
@@ -0,0 +1,15 @@
+package com.example.LocalDelicacies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CityListActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
